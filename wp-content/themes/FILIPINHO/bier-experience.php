a0e674fdcee682efa31fc65d2d7d692656d7e233
Wesley <wesleypetitemberte@live.com>
2019-11-28 03:46:00 -0300

    royal pub and kombi

---
+++ wp-content/themes/FILIPINHO/bier-experience.php
@@ -0,0 +1,29 @@
+<section class="bier-experience">
+
+    <img class="bg-img" src="<?php echo get_template_directory_uri(); ?>/dist/img/image.png" alt="Imagem de Fundo">
+    <img class="bg-div" src="<?php echo get_template_directory_uri(); ?>/dist/img/image 8.png" alt="Imagem do Pub">
+    <img class="bg-img-mobile" src="<?php echo get_template_directory_uri(); ?>/dist/img/BG.png" alt="Imagem de Fundo Mobile">
+    <div class="header-bier">
+        <h1>BIER</h1>
+        <span>EXPERIENCE</span>
+        <p>DESCUBRA O PRAZER DE LOREM IPSUM</p>
+
+        <img class="dado-img" src="<?php echo get_template_directory_uri(); ?>/dist/img/DADO.png" alt="Imagem do Dado">
+    </div>
+
+    <div class="pub-and-kombi">
+        <div class="item">
+            <div class="text">
+                ROYAL<br>BLACK<br>
+                <span class="text-strong">PUB</span>
+            </div>
+            <img class="item-img" src="<?php echo get_template_directory_uri(); ?>/dist/img/mask-group.png" alt="Imagem do Pub">
+        </div><!--
+
+        --><div class="item">
+            <div class="text"><span class="text-strong">KOMBI</span></div>
+            <img class="item-img" src="<?php echo get_template_directory_uri(); ?>/dist/img/kombi.png" alt="Imagem da Kombi">
+        </div>
+    </div>
+
+</section>
