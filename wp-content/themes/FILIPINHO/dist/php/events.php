d537b491c58385488fa42e90f64205b2217ce49e
Wesley <wesleypetitemberte@live.com>
2019-12-10 18:38:11 -0300

    hall start

---
+++ wp-content/themes/FILIPINHO/dist/php/events.php
@@ -0,0 +1,43 @@
+<section class="events">
+    <div class="header-events">
+        <h1>EVENTOS</h1>
+        <span>OFERECEMOS UMA COMPLETA ESTRUTURA DE EVENTOS LOREM IPSUM.</span>
+    </div>
+
+    <div class="halls">
+        <div class="hall-belgian">
+            <img src="<?php echo get_template_directory_uri(); ?>/dist/img/belgian-ale.png" alt="Imagem da Belgian Ale">
+
+            <div class="main-text">
+                <span class="title-orange">SALÃO</span>
+                <span class="title-white">BELGIAN ALE</span>
+                <span class="text-white">Ambiente agradável para receber os amigos e a família, com uma decoração em madeira tratada, que possui um charme todo especial.</span>
+
+                <div class="saiba-mais">
+                    <span>SAIBA MAIS</span>
+                </div>
+            </div>
+        </div>
+
+        <div class="hall-royal">
+            <img src="<?php echo get_template_directory_uri(); ?>/dist/img/royal-black.png" alt="Imagem da Royal Black">
+
+            <div class="main-text">
+                <span class="title-orange">SALÃO</span>
+                <span class="title-white">ROYAL BLACK</span>
+                <span class="text-white">Ambiente agradável para receber os amigos e a família, com uma decoração em madeira tratada, que possui um charme todo especial.</span>
+            </div>
+        </div>
+
+        <div class="hall-ilex">
+            <img src="<?php echo get_template_directory_uri(); ?>/dist/img/ilex-img.png" alt="Imagem da Ilex">
+
+            <div class="main-text">
+                <span class="title-orange">SALÃO</span>
+                <span class="title-white">ILEX</span>
+                <span class="text-white">Ambiente agradável para receber os amigos e a família, com uma decoração em madeira tratada, que possui um charme todo especial.</span>
+            </div>
+        </div>
+    </div>
+
+</section>
--- wp-content/themes/FILIPINHO/index.php
+++ wp-content/themes/FILIPINHO/index.php
@@ -3,4 +3,5 @@
 <?php get_template_part("portfolio");?>
 <?php get_template_part("bier-experience");?>
 <?php get_template_part("restaurants");?>
+<?php get_template_part("events");?>
 <?php get_footer(); ?>
