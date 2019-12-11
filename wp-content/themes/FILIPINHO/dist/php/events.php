70fb7559f14c3b1bbc7837edf30266408714565d
Wesley <wesleypetitemberte@live.com>
2019-12-11 18:31:00 -0300

    events in progress

--- wp-content/themes/FILIPINHO/dist/php/events.php
+++ wp-content/themes/FILIPINHO/dist/php/events.php
@@ -5,13 +5,13 @@
     </div>
 
     <div class="halls">
-        <div class="hall-belgian">
-            <img src="<?php echo get_template_directory_uri(); ?>/dist/img/belgian-ale.png" alt="Imagem da Belgian Ale">
+        <div class="hall">
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/img/belgian-ale.png" alt="Imagem da Belgian Ale">
 
             <div class="main-text">
-                <span class="title-orange">SALÃO</span>
-                <span class="title-white">BELGIAN ALE</span>
-                <span class="text-white">Ambiente agradável para receber os amigos e a família, com uma decoração em madeira tratada, que possui um charme todo especial.</span>
+                <h3>SALÃO</h3>
+                <h4>BELGIAN ALE</h4>
+                <p>Ambiente agradável para receber os amigos e a família, com uma decoração em madeira tratada, que possui um charme todo especial.</p>
 
                 <div class="saiba-mais">
                     <span>SAIBA MAIS</span>
@@ -19,23 +19,25 @@
             </div>
         </div>
 
-        <div class="hall-royal">
-            <img src="<?php echo get_template_directory_uri(); ?>/dist/img/royal-black.png" alt="Imagem da Royal Black">
+        <div class="hall-royal-ilex">
+            <div class="hall">
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/img/royal-black.png" alt="Imagem da Royal Black">
 
-            <div class="main-text">
-                <span class="title-orange">SALÃO</span>
-                <span class="title-white">ROYAL BLACK</span>
-                <span class="text-white">Ambiente agradável para receber os amigos e a família, com uma decoração em madeira tratada, que possui um charme todo especial.</span>
+                <div class="main-text">
+                    <h3>SALÃO</h3>
+                    <h4>ROYAL BLACK</h4>
+                    <p>Ambiente agradável para receber os amigos e a família, com uma decoração em madeira tratada, que possui um charme todo especial.</p>
+                </div>
             </div>
-        </div>
 
-        <div class="hall-ilex">
-            <img src="<?php echo get_template_directory_uri(); ?>/dist/img/ilex-img.png" alt="Imagem da Ilex">
+            <div class="hall">
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/img/ilex-img.png" alt="Imagem da Ilex">
 
-            <div class="main-text">
-                <span class="title-orange">SALÃO</span>
-                <span class="title-white">ILEX</span>
-                <span class="text-white">Ambiente agradável para receber os amigos e a família, com uma decoração em madeira tratada, que possui um charme todo especial.</span>
+                <div class="main-text">
+                    <h3>SALÃO</h3>
+                    <h4>ILEX</h4>
+                    <p>Ambiente agradável para receber os amigos e a família, com uma decoração em madeira tratada, que possui um charme todo especial.</p>
+                </div>
             </div>
         </div>
     </div>
