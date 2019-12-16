efbee4cf0bc3da7934053aac9cdac74f96121efa
Wesley <wesleypetitemberte@live.com>
2019-12-16 18:40:04 -0300

    section connect

--- wp-content/themes/FILIPINHO/index.php
+++ wp-content/themes/FILIPINHO/index.php
@@ -5,4 +5,5 @@
 <?php get_template_part("restaurants");?>
 <?php get_template_part("events");?>
 <?php get_template_part("shop");?>
+<?php get_template_part("connect-dado");?>
 <?php get_footer(); ?>
